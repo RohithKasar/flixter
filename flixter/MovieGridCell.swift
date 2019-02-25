169c44fa1b6965b2b560b4822324be382611c23f
Rohith Kasar <mooted@kasars-mbp.dynamic.ucsd.edu>
2019-02-24 20:01:15 -0800

    week 2 changes

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Rohith Kasar on 2/19/19.
+//  Copyright © 2019 rohith.kasar. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
